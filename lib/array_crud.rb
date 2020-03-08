8bf3176dbd73d59e82ad80638abaae2ce09cc74a
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:23:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   array << ["wow", "I", "am", "really", "learning"]
+   array = ["wow", "I", "am", "really", "learning"]
     element << "arrays!"
   
 end
