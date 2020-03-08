4d35f8b4d939fba69faa2922b9275e7878ebfb49
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:39:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  ar = ["I", "am", "really", "learning", "arrays!"]
   array = element.pop
   
 end
