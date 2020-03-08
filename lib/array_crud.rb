5b68a202213bb49b21f69bd425d294006bb8fd69
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:49:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  element = ["wow", "I", "am", "really", "learning", "arrays!"]
+  
   
 end
 
