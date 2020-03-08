0bd660b58872a703096695acd53521f73ca04d8d
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:36:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,6 +21,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  element = ["I", "am", "really", "learning", "arrays!"]
+  element.
   
 end
 
