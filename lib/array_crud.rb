605e16f80c079e9c2463746495b29b68537fa842
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:30:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   array = ["wow", "I", "am", "really", "learning"]
+   element = ["wow", "I", "am", "really", "learning"]
     puts array.inspect
   
 end
