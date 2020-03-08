7511119fc69a47225a3d247381381a43d087ce68
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:41:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  element = 
+  element = ["wow", "I", "am", "really", "learning", "arrays!"]
+  
   
 end
 
