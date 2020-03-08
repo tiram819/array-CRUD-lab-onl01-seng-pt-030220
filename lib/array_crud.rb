824550cc20c472426e2e0b6149519e81ae4341fb
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 02:33:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  element = 
+  element = ["I", "am", "really", "learning"]
+  element.unshift
   
 end
 
